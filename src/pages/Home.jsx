e02c5ba2f888211693eec6cb8ef63d4727ce9122
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-07-10 13:02:27 -0400

    home page
    main page should be responsive now

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
+import "../css/Home.css";
 
 const Home = () => {
     return (
         <>
-        <Header />
+            <Header />
+            <main>
+                <h2>Recoursive</h2>
+                <a href="/programs" className="top-link">Learn more about our programs</a>
+                <a href="/register" className="top-link">Reserve a class</a>
+            </main>
         </>
     )
 }
