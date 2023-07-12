a9b8feb857f605c7eb5fed10c4a610ee7222886e
Jake Park <jakepark2908@gmail.com>
2023-07-12 19:55:07 -0400

    need some fixing on the card deck, for some reason it does not scroll when using card deck again to create another row

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
-
+//imported home.css to keep a general theme(?)
+import "../css/Home.css";
 const Contact = () => {
     return (
         <>
         <Header />
+        <main>
+            <h2>Contacts</h2>
+        </main>
         </>
     )
 }
--- src/pages/Programs.jsx
+++ src/pages/Programs.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import Header from "../components/Header";
-
+//imported home.css to keep a general theme(?)
+import "../css/Home.css";
+//external other css for the card stuff
+import "../css/Programs.css";
 const Programs = () => {
     return (
         <>
-        <Header />
+            <Header />
+            <main>
+                {/* WARNING: IF WANT ANOTHER ROW NEED TO CREATE ANOTHER CARD DECK */}
+                <h2>Programs</h2>
+                <div class="card-deck">
+                    <div className="card">
+                        <div className="card-body">
+                            <h4 class="title">Scratch</h4>
+                            <p>Description</p>
+                            <p>Date:</p>
+                            <p>Location:</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-body">
+                            <h4 class="title">Python</h4>
+                            <p>Description</p>
+                            <p>Date:</p>
+                            <p>Location:</p>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </>
     )
 }
