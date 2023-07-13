0ea0513d7f6ca6a119bcc1578014b7296e4d9cc5
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-07-13 08:24:29 -0400

    add actual program descriptions 👀

--- src/pages/Programs.jsx
+++ src/pages/Programs.jsx
@@ -15,17 +15,17 @@ const Programs = () => {
                 <div className="card-deck">
                     <Card 
                         title="Scratch"
-                        description="Scratch is a block-based visual programming language and online community targeted primarily at children. Scratch is really fun and I love Scratch haha just making this sentence realllyly resaallly long erfife rnew line hahahd eiwnd ewoi ew fowefowefrnew line hahahd eiwnd ewoi ew "
+                        description="Scratch is a fun and easy to use block-based programming language. Developed at MIT, it was created to make fundamental programming concepts accessible to young learners."
                         image={ScratchLogo}
                     />
                     <Card 
                         title="Python"
-                        description="Scratch is a block-based visual programming language and online community targeted primarily at children."
+                        description="Python is a high-level, general-purpose programming language. It has a simple, beginner friendly syntax, but has advanced applications in the real world for machine learning, data science, and much more."
                         image={PythonLogo}
                     />
                     <Card 
                         title="Web Development"
-                        description="Scratch is a block-based visual programming language and online community targeted primarily at children."
+                        description="As more and more technology moves into the web browser, learning HTML, CSS, & Javascript becomes more and more powerful. Driving the internet revolution, these technologies can create anything — from beautiful interfaces to powerful web applications."
                         image={WebDevLogo}
                     />
                 </div>
