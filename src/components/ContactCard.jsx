70fa62ee46ecc4d9059667275c0cf88ba6802874
Jake Park <115760042+3dgyGamer@users.noreply.github.com>
2023-07-17 11:43:53 -0400

    Contact page (#4)
    * added content to contact page. added contactCard component. changed the header link name for contact from contact us to just contact.
    
    * made a small change so copy and paste is available for email address
    
    * change padding a bit for the link so it doesn't look weird
    
    * a small link fix to ensure that when other social links are added no errors due to mailto
    
    * tweak colors and add defined border/bg for contactCard
    
    * update contactCard with new linkText prop
    
    ---------
    
    Co-authored-by: mudasir <96320211+mud-ali@users.noreply.github.com>

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -7,7 +7,7 @@ const ContactCard = (props) => {
             <div className="contactCard-body">
                 <h4 className="title">{props.title}</h4>
                 <p>{props.description}</p>
-                <a href = {"mailto: ${props.link}"}>{props.link}</a>
+                <a href = {props.link}> {props.linkText}</a>
             </div>
         </div>
     )
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -10,7 +10,6 @@ const Register = () => {
             <div className="jumbotron">Reserve a class</div>
             <p className="">Thank you for choosing us! Please fill out the form below.</p>
             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc0e9gD_qmu1xh1e52xPsjk7jUOhk13OW-QlKsHksbi7e-eTQ/viewform?embedded=true" width="640" height="2774" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-
         </main>
         </>
     )
